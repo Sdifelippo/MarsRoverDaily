82d450eabd27668ce8bb2f6af026a0a47f0b9268
Steven Difelippo <matrixdriver04@gmail.com>
2017-08-29 23:43:50 -0400

    first commit file structure and setup

---
+++ src/components/GetImageForm.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import "../styles/App.css";
+
+class GetImageForm {
+  constructor() {
+
+  }
+}
+
+export default GetImageForm;
---
+++ src/components/ImageDisplay.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import "../styles/App.css";
+
+class ImageDisplay {
+  constructor() {
+
+  }
+}
+
+export default ImageDisplay;
